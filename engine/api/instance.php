673e4152cbd2911f3d319ff0cdc4d6b1308ae2bc
Akito Iwakura <aki@asterleen.com>
2019-05-27 00:01:53 +0300

    Implement file gathering API

--- engine/api/instance.php
+++ engine/api/instance.php
@@ -1,16 +1,40 @@
 <?php
 
-function processAdd() {
-	$instance_tag = (int)$_POST['instance_tag'];
-	
-	json_respond(0, 'OK');
+function processGetCode() {
+	$instanceTag = $_GET['instance_tag'];
+	if (!validateInstanceTag($instanceTag)) {
+		json_respond(1, 'Bad instance tag');
+	}
+
+	$codePath = '';
+
+	switch ($_GET['type']) {
+		case 'liquidsoap':
+			$codePath = WM_DATA_DIR . '/data/scripts/' . $instanceTag . '.liq';
+			break;
+
+		case 'icecast':
+			$codePath = WM_DATA_DIR . '/data/icecast/' . $instanceTag . '.xml';
+			break;
+
+		default:
+			json_respond(1, 'Bad instance type tag');
+	}
+
+	$code = file_get_contents($codePath);
+
+	if (empty($code)) {
+		json_respond(2, 'Empty or non-existent code file');
+	} else {
+		json_respond(0, $code);
+	}
 }
 
 array_shift($route); // remove /instance/
 switch ($route[0])
 {
-	case 'add' :
-		processAdd();
+	case 'code' :
+		processGetCode();
 		break;
 
 	default:
